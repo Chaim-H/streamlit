349d52ab2f91ea193588095e2268c39eda4b7638
Chaim-H <73102984+Chaim-H@users.noreply.github.com>
2022-01-23 21:28:20 +0200

    Update app_solids_accumulation_pipes.py

--- app_solids_accumulation_pipes.py
+++ app_solids_accumulation_pipes.py
@@ -78,8 +78,10 @@ def main():
       
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">(based on machine learning model)</h1> 
+    <h1 style ="color:black;text-align:center;">(Based on a machine learning model (Harpaz et al., under review))</h1> 
     </div> 
+    
+    
     """
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html = True) 
@@ -91,37 +93,33 @@ def main():
     0.00000000e+00, 6.63697100e-02, 4.45434000e-04, 1.07529531e+03,
     7.29700590e-02, 1.39022858e+01, 1.00000000e+00
     ]])
-    x[0][0]  = st.select_slider('Select a diameter of pipe [m]:', options=[0.1882, 0.2354, 0.2966, 0.3766, 0.4708, 0.5932])
-    x[0][1] = st.slider('Select a slope of pipe [%]:', min_value=0.29, max_value=10.01, value=(4.42))/100
+    x[0][0]  = st.select_slider('Diameter of the pipe [m]:', options=[0.1882, 0.2354, 0.2966, 0.3766, 0.4708, 0.5932])
+    x[0][1] = st.slider('Slope of the pipe [%]:', min_value=0.29, max_value=10.01, value=(4.42))/100
     x[0][3] = 1 #st.slider("What is Type reduction                ?", min_value=0.0000, max_value=1.0000, value=(1.0000))
-    x[0][4] = st.slider("Select a stream order of pipe:", min_value=1, max_value=75, value=(1))
-    x[0][5] = st.slider("Select a pipe residents of pipe", min_value=9, max_value=62146, value=(106))
-    x[0][6] = st.slider("What is Aspect ratio of network? [-]", min_value=0.133, max_value=7.5, value=(0.83))
-    x[0][7] = st.slider("What is Density of network? [person/km^2]", min_value=4505, max_value=32060, value=(26210))/1000
-    x[0][8] = st.slider("Select a Betweenness centrality of pipe [-]:", min_value=0.0000, max_value=0.7041, value=(0.0000))
-    x[0][9] = st.slider("Select a Closeness centrality of pipe [-]:", min_value=0.0209, max_value=0.1944, value=(0.0664))
-    x[0][10] = st.slider("Select a Current flow closeness centrality of pipe [-]:", min_value=0.01, max_value=0.40, value=(0.04))/100
-    x[0][11] = st.slider("Select a Second order centrality of pipe [-]:", min_value=129.0504, max_value=4789.9608, value=(1075.2953))
-    x[0][12] = st.slider("Select a Katz centrality of pipe [-]:", min_value=0.0514, max_value=0.1742, value=(0.0730))
-    x[0][13] = st.slider("Select a Harmonic centrality of pipe [-]:", min_value=5.7243, max_value=28.8047, value=(13.9023))
-    x[0][14] = st.slider("Select a Node degree of pipe [-]:", min_value=1, max_value=5, value=(1))
+    x[0][4] = st.slider("Stream order of the pipe [-]:", min_value=1, max_value=75, value=(1))
+    x[0][5] = st.slider("Number of residents discharging wastewater to the pipe [-]:", min_value=9, max_value=62146, value=(106))
+    x[0][8] = st.slider("Betweenness centrality of the pipe [-]:", min_value=0.0000, max_value=0.7041, value=(0.0000))
+    x[0][9] = st.slider("Closeness centrality of the pipe [-]:", min_value=0.0209, max_value=0.1944, value=(0.0664))
+    x[0][10] = st.slider("Current flow closeness centrality of the pipe [-]:", min_value=0.01, max_value=0.40, value=(0.04))/100
+    x[0][11] = st.slider("Second order centrality of the pipe [-]:", min_value=129.0504, max_value=4789.9608, value=(1075.2953))
+    x[0][12] = st.slider("Katz centrality of the pipe [-]:", min_value=0.0514, max_value=0.1742, value=(0.0730))
+    x[0][13] = st.slider("Harmonic centrality of the pipe [-]:", min_value=5.7243, max_value=28.8047, value=(13.9023))
+    x[0][14] = st.slider("Node degree of the pipe [-]:", min_value=1, max_value=5, value=(1))
+    x[0][6] = st.slider("Aspect ratio of the network [-]:", min_value=0.133, max_value=7.5, value=(0.83))
+    x[0][7] = st.slider("Density of the network? [person/km\u00b2]", min_value=4505, max_value=32060, value=(26210))/1000
     
-    st.write('Parameters for prediction:')
-    x[0][2] = st.slider("What is DWES Scenario:", min_value=0.1000, max_value=1.0000, value=(0.7000), step=0.1)
-    threshold = st.number_input("Set a threshold value (diurnal max shear stress) below which the pipe is defined as solid-accumulated \n(2 in the literature)",min_value=0.0, max_value=100.0, value=(2.0)) 
-
-    
+#     st.write('Parameters for prediction:')
+    x[0][2] = st.slider("Proportion of reduction in wastewater flow (due to DWES scenario) [-]:", min_value=0.1000, max_value=1.0000, value=(0.7000), step=0.1)
+    threshold = st.number_input("Threshold value of the maximum shear stress below the pipe will accumulate solids (2 Pa typical value) [Pa]:",min_value=0.0, max_value=100.0, value=(2.0)) 
+  
     result =""
-      
     # when 'Predict' is clicked, make the prediction and store it 
-    if st.button("Predict"): 
-        
+    if st.button("Predict"):
         result = prediction(x, threshold, regressor, pca, train_location, point_round)
-        
         if result[1] != "":
             st.success('{}'.format(result[1]))
         st.success('Your pipe {}'.format(result[0]))
-#         print(LoanAmount)
+    st.write('Harpaz C., Russo S., Leitão J.P., Penn  R., (under review). Potential of supervised machine learning algorithms for estimating the impact of water efficient scenarios on solids accumulation in sewers')
      
 if __name__=='__main__': 
     main()
